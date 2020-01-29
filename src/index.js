cb3af4e2704b092f112039ad904a0f3ff2569528
abespalov <prosvet_egor@mail.ru>
2020-01-30 01:00:00 +0300

    Example of use-effect React Hooks

--- src/index.js
+++ src/index.js
@@ -1,20 +1,57 @@
-import React, { useContext } from 'react';
+import React, {useState, useEffect, Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const MyContext = React.createContext();
-
 const App = () => {
-    return(
-        <MyContext.Provider value='How low'>
-            <Child />
-        </MyContext.Provider>
-    );
+    const [value, setValue] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    if(visible){
+        return(
+            <div style={{fontSize: '25px'}}>
+                <button onClick={() => setValue((v) => v + 1)}>+</button>
+                <button onClick={() => setVisible(false)}>hide</button>
+                <ClassCounter value={value} />
+                <HookCounter value={value} />
+            </div>
+        );
+    } else{
+        return(
+            <div>
+                <button onClick={() => setVisible(true)}>show</button>
+            </div>
+        );
+    }   
 }
 
-const Child = () => {
-    const value = useContext(MyContext);
+class ClassCounter extends Component {
 
-    return <h1>{value}</h1>;
+    componentDidMount() {
+        console.log('class: mount');
+    }
+
+    componentDidUpdate(){
+        console.log('class: update');
+    }
+
+    componentWillUnmount(){
+        console.log('class: unmount');
+    }
+
+    render() {
+        return (
+            <p>{this.props.value}</p>
+        )
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));  
+const HookCounter   = ({value}) => {
+
+    useEffect(() => {
+        console.log(' useEffect()');
+        return () => console.log('clear');
+    }, [value]);
+
+    return <p> {value} </p>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/use-context.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import ReactDOM from 'react-dom';
+
+const MyContext = React.createContext();
+
+const App = () => {
+    return(
+        <MyContext.Provider value='How low'>
+            <Child />
+        </MyContext.Provider>
+    );
+}
+
+const Child = () => {
+    const value = useContext(MyContext);
+
+    return <h1>{value}</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));   
